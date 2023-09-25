dd778f4525c72286a6ff72259c5c8d01eacd2e23
Muhammad Farhan Gultom <pitjarusfarhan@outlook.com>
2023-09-25 19:21:58 +0700

    Initial commit

---
+++ app/src/main/java/dev/diprojectin/esystem/ui/sign_in/SignInActivity.kt
@@ -0,0 +1,12 @@
+package dev.diprojectin.esystem.ui.sign_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.diprojectin.esystem.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
